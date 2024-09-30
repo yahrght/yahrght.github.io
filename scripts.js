ffc631163442e94e00881a3f912f7f0b4a963ba8
yas <yahrght606@gmail.com>
2024-09-30 13:01:20 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function checkPassword(event) {
+    event.preventDefault(); // Prevent link from following the URL
+    var password = prompt("Enter the password:");
+    if (password === "yourPassword") { // Replace 'yourPassword' with your actual password
+        window.location.href = "https://yahrght.github.io/cheering"; // Redirect to the protected link
+    } else {
+        alert("Incorrect password");
+    }
+}
