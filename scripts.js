1f48b7b0a19a39ee9737b433d678476070338c35
yas <yahrght606@gmail.com>
2024-09-30 13:04:25 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function checkPassword(event) {
     event.preventDefault(); // Prevent link from following the URL
     var password = prompt("Enter the password:");
-    if (password === "yourPassword") { // Replace 'yourPassword' with your actual password
+    if (password === "qarabag") { // Replace 'yourPassword' with your actual password
         window.location.href = "https://yahrght.github.io/cheering"; // Redirect to the protected link
     } else {
         alert("Incorrect password");
